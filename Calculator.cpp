16f1388f52cdade12ef35cec30a7857c83c33057
Alienszxck <72641682+Alienszxck@users.noreply.github.com>
2020-10-10 23:27:28 +0800

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ int main () {
+ 	int c,j;
+ 	int sum,diff,product,qoutient;
+ 	cout<<"Enter 1st number: ";
+ 	cin>>c;
+ 	cout<<"Enter 2nd number: ";
+ 	cin>>j;
+ 	cout<<endl;
+ 	sum = c+j;
+ 	diff = c-j;
+ 	product = c*j;
+ 	qoutient = c/j;
+ 	cout<<"The Sum of 1st number & 2nd number is -> "<<sum<<endl;
+ 	cout<<"The Difference of 1st number & 2nd number is -> "<<diff<<endl;
+ 	cout<<"The Product of 1st number & 2nd number is -> "<<product<<endl;
+ 	cout<<"The Qoutient of 1st number & 2nd number is -> "<<qoutient<<endl;
+}
